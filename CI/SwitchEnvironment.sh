af6aebd49d4fafc7aaf7f26339b0a3b2b2ea090d
Matheus Amazonas <matheus.amazonas@gmail.com>
2024-04-12 09:33:59 +0200

    🎉 Add empty project

---
+++ CI/SwitchEnvironment.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+uname="$(uname -s)"
+
+PROJECT_PATH=$PROJECT_DIRECTORY
+
+# Windows Cloud Build machines run Cygwin, which won't return a good PROJECT_DIRECTORY.
+# This workaround was suggested by a Unity Services Support agent.
+if [[ "$uname" == "CYGWIN"* ]]; then
+    PROJECT_PATH=$(cygpath -wa "$PROJECT_DIRECTORY")
+fi
+
+# Set enrivonment
+"$UNITY_EXE" \
+    -batchmode \
+    -projectPath $PROJECT_PATH \
+    -logfile - \
+    -executeMethod CloudBuildTest.CommandLineBuild.SetEnvironment
+
+exit $?
